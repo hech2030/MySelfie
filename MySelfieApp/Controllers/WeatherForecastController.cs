8ddfec243f2a5a3c2c310d0c9e035415993d269c
hech2030 <hichemfakhfakh@hotmail.fr>
2023-03-20 15:00:01 +0100

    chore: test

--- MySelfieApp/Controllers/WeatherForecastController.cs
+++ MySelfieApp/Controllers/WeatherForecastController.cs
@@ -21,13 +21,14 @@ namespace MySelfieApp.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var testGitChanges =  Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+            return testGitChanges;
         }
     }
 }
